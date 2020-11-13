6b69e7809694c31f736e75e1d5ee2ff2d6541044
Chahine Benlahcen <chahine.benlahcen@gmail.com>
2020-11-13 10:20:13 +0100

    exo1

--- ep01.js
+++ ep01.js
@@ -1,4 +1,12 @@
+const EventEmitter = require('events')
+const eventEmitter = new EventEmitter()
+
 function empty() {
 
+    eventEmitter.on('start', () => {
+        console.log('hi')    
+    })
+    eventEmitter.emit('start')
     
 }
+empty()
